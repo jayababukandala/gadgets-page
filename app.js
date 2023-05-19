33d5091d5af5902e43de7fb36fd54a6314f360f9
jayababukandala <joykandala@gmail.com>
2023-05-19 04:52:01 +0000

    sending response to gadgets file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/gadgets", (request, response) => {
-  response.sendFile("./gadgets.html", { root: --dirname });
+  response.sendFile("./gadgets.html", { root: __dirname });
 });
 
 module.exports = app;
